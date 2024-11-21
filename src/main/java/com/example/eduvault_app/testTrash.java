c64a1c1c4094206ab4fa998fb75d9dc6056d1b82
Kateht <tranb2203588@student.ctu.edu.vn>
2024-11-21 19:48:55 +0700

    change connection

--- src/main/java/com/example/eduvault_app/testTrash.java
+++ src/main/java/com/example/eduvault_app/testTrash.java
@@ -2,13 +2,14 @@ package com.example.eduvault_app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class testTrash extends Application{
     public void start(Stage primaryStage) throws Exception {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("delete_test.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/eduvault_app/trash.fxml"));
             Scene scene = new Scene(loader.load());
             primaryStage.setScene(scene);
             primaryStage.setTitle("Delete Test");
